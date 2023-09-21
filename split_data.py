85c4d526e3af6cde8f7c8c6f1cbaa18ec56ba9b8
Your Name <you@example.com>
2023-09-21 20:40:02 +0700

    update

--- split_data.py
+++ split_data.py
@@ -1,81 +1,94 @@
-import pandas as pd 
-df = pd.read_parquet(f'data/cmg-data-processed.parquet', engine='fastparquet')
-# df = df.sort_values(by=['extract_level'])
-print(df.head(1)['index'])
-result = list()
-cms = set(df['index'])
 from tqdm import tqdm
-for idx in tqdm(cms):
-    rows = df[df['index']==idx]
-    index = idx.replace('_file_fc_patch.csv','')
+
+DATA_DIR = 'data/cmg-data'
+
+import pandas as pd 
+df = pd.read_parquet(f'{DATA_DIR}/cmg-data-processed.parquet', engine='fastparquet')
+print(f'Num of commits:', df['index'].nunique())
+
+with open('cms.txt') as f:
+    cms = [l.strip() for l in f.readlines()]
+
+def get_id(dir_path='cmg-data/split-data', type='randomly'):
+    with open(f'{dir_path}/{type}/train_id.txt') as file:
+        train_val_id = [line.rstrip() for line in file]
+    with open(f'{dir_path}/{type}/test_id.txt') as file:
+        test_id = [line.rstrip() for line in file]
+        
+    test_id = list(set(cms).intersection(set(test_id)))
+    train_val_id = list(set(cms).intersection(set(train_val_id)))
+
+    df = pd.DataFrame(train_val_id, columns=['index'])
+    train_percent = 0.9
+    train_id = list(df['index'].sample(int(df['index'].nunique() - 1000), random_state=42))
+    val_id = list(df[~df['index'].isin(train_id)]['index'])
+
+    print(f'Training size: {len(train_id)}   |   Val size: {len(val_id)}   |   Test size: {len(test_id)}')
+    
+    return train_id, val_id, test_id
+
+train_id, val_id, test_id = get_id(dir_path=f'{DATA_DIR}/split-data', type='cross_project')
+# train_id, val_id, test_id = get_id(dir_path=f'{DATA_DIR}/split-data')
+
+df_type = pd.read_csv('meta_patch_db.csv')
+type_dict = dict()
+for _,row in df_type.iterrows():
+    index = str(row['commit_id'])
+    index = index.lower()
+    type_dict[index] = 1 if row['category'] == 'security' else 0 
+    
+data = list()
+index_list = set(df['index'])
+
+for id in tqdm(index_list):
+    df_commit = df[df['index']==id]
     codes = list()
-    for _, row in rows.iterrows():
+    for _, row in df_commit .iterrows():
         diff = row['change_abstract']
-        # for l in row['diff'].splitlines():
-        #     if l.startswith('-') or l.startswith('+'):
-        #         diff.append(l)
-        doc = row['msg_change_abstract'].split()
+        msg = row['msg_change_abstract'].split()
         if row['old_path_file'] == row['new_path_file']:
-            file = row['new_path_file']
+            file_name = row['new_path_file']
         else:
             if row['old_path_file'] is not None and row['new_path_file'] is not None:
-                file = row['old_path_file'] + ' SEP ' + row['new_path_file']
+                file_name = row['old_path_file'] + ' SEP ' + row['new_path_file']
             else:
-                file = row['old_path_file'] if row['old_path_file'] is not None else row['new_path_file']
-        file = file+ ' SEP'
-        code = file + diff
+                file_name = row['old_path_file'] if row['old_path_file'] is not None else row['new_path_file']
+
+        code = file_name + ' SEP ' + diff
         code = code.split() + ["SEP"]
         codes.extend(code)
-    result.append({'code_tokens':codes,'docstring_tokens':doc,'index':index})
-import random 
-with open('data/split-data/randomly/train_id.txt') as f:
-    train_id = [l.strip() for l in f.readlines()]
-with open('data/split-data/randomly/test_id.txt') as f:
-    test_id = [l.strip() for l in f.readlines()]
-with open('data/split-data/randomly/valid_id.txt') as f:
-    val_id = [l.strip() for l in f.readlines()]
+    commit_id = id.split('_')[-1]
+
+    if commit_id in type_dict.keys():
+        type = type_dict[commit_id]
+    else:
+        type = 1
+
+    data.append({'code_tokens': codes, 
+                 'docstring_tokens': msg, 
+                 'index': id.replace('_file_fc_patch.csv',''), 
+                 'type': type
+                })
+
+
+
+len(data)
 train,test,val = [],[],[]
-for el in result:
+for el in data:
     if el['index'] in train_id:
         train.append(el)
     elif el['index'] in val_id:
         val.append(el)
     else:
         test.append(el)
+
 import json
 def dump_to_file(obj, file):
-    with open(file,'w+') as f:
+    with open(file, 'w') as f:
         for el in obj:
             f.write(json.dumps(el)+'\n')
-dump_to_file(train,'data/train3.jsonl')
-dump_to_file(test,'data/test3.jsonl')
-dump_to_file(val,'data/valid3.jsonl')
 
-
-import pandas as pd 
-df = pd.read_csv('meta_patch_db.csv')
-df.head(1)
-type_dict = dict()
-for _,row in df.iterrows():
-    index= str(row['commit_id'])
-    index = index.lower()
-    type_dict[index] = 1 if row['category'] == 'security' else 0 
-
-import json
-result = list()
-# replace with train.valid set
-file = 'data/valid.jsonl'
-c = 0
-with open(file) as f:
-    for l in f.readlines():
-        data = json.loads(l.strip())
-        index = data['index'].split('_')[-1]
-        if index in type_dict.keys():
-            data['type'] = type_dict[index]
-            c += 1
-        else:
-            data['type'] = 1
-        result.append(json.dumps(data))
-with open(file,'w+') as f:
-    f.write('\n'.join(result))
-print(len(result),c)
+print(len(train), len(val), len(test))
+dump_to_file(train,f'{DATA_DIR}/train.jsonl')
+dump_to_file(test,f'{DATA_DIR}/test.jsonl')
+dump_to_file(val,f'{DATA_DIR}/valid.jsonl')
